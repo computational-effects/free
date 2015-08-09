f9a069791a6875ac97451a4b1daf6ca50bcc2750
joneshf <jones3.hardy@gmail.com>
2015-08-09 12:19:30 -0700

    Extract Free to its own package
    Added KVS example.

---
+++ examples/kvs/index.js
@@ -0,0 +1,154 @@
+'use strict';
+
+var pbp = require('pbp');
+var mixins = require('ce-mixin').mixins;
+var Catamorphism = require('ce-catamorphism').Catamorphism;
+var Stringify = require('ce-stringify').Stringify;
+var Unit = require('ce-unit').Unit;
+var R = require('ramda');
+
+var value = pbp.value;
+var enumerableGet = pbp.enumerableGet;
+var enumerableValue = pbp.enumerableValue;
+
+var FreeDerive = require('../../').FreeDerive;
+
+/*
+  A Key value store should need only three primitives:
+
+  1. Get
+  2. Put
+  3. Del
+
+  From this most other things should be able to be implemented.
+
+  The simplest approach involves an encoding like:
+
+  data KVS a = Get String | Put String a | Del String
+
+  The problem with this approach is that if we want a set of instructions,
+  we're pretty much limited to a list, yet we can't easily use previous results.
+
+  Encoding it with continuations is a better approach:
+
+  data KVS val a = Get String (val -> a) | Put String val a | Del String a
+
+  With this, we can then use Free to sequence instructions easily.
+*/
+var KVS = mixins([Catamorphism, FreeDerive, Stringify],
+  Object.create(null)
+);
+
+function Get(str, cont) {
+  return Object.create(KVS, {
+    type: value('Get'),
+    args: value([str, cont]),
+    ctor: value(Get),
+    map: enumerableValue(function(f) {
+      return Get(str, function(val) {
+        return f(cont(val));
+      });
+    }),
+  });
+}
+
+function Put(str, val, cont) {
+  return Object.create(KVS, {
+    type: value('Put'),
+    args: value([str, val, cont]),
+    ctor: value(Put),
+    map: enumerableValue(function(f) {
+      return Put(str, val, f(cont));
+    }),
+  });
+}
+
+function Del(str, cont) {
+  return Object.create(KVS, {
+    type: value('Del'),
+    args: value([str, cont]),
+    ctor: value(Del),
+    map: enumerableValue(function(f) {
+      return Del(str, f(cont));
+    }),
+  });
+}
+
+function get(str) {
+  return Get(str, function(x) { return x; }).liftF;
+}
+
+function put(str, val) {
+  return Put(str, val, Unit).liftF;
+}
+
+function del(str) {
+  return Del(str, Unit).liftF;
+}
+
+/*
+  Using the primitives, we can extend the dsl as though it's a regular `Monad`.
+*/
+function modify(str, f) {
+  return get(str).chain(function(val) {
+    return put(str, f(val));
+  });
+}
+
+/*
+  Now we can interpret this in a pure object context.
+*/
+
+function pureObject(free, store) {
+  return free.cata({
+    Of: function(_) { return store; },
+    Join: function(kvs) {
+      return kvs.cata({
+        Get: function(str, cont) {
+          return pureObject(cont(store[str]), store);
+        },
+        Put: function(str, val, cont) {
+          return pureObject(cont, R.assoc(str, val, store));
+        },
+        Del: function(str, cont) {
+          return pureObject(cont, R.dissoc(str, store));
+        },
+      });
+    },
+  });
+}
+
+/*
+  We can also interpret this in a mutable object context.
+*/
+
+function mutableObject(free, store) {
+  return free.goCata({
+    Get: function(str, cont) {
+      return cont(store[str]);
+    },
+    Put: function(str, val, cont) {
+      store[str] = val;
+      return cont;
+    },
+    Del: function(str, cont) {
+      delete store[str];
+      return cont;
+    },
+  });
+}
+
+/*
+  Now, if we write a script, we can use whatever interpreter we want.
+*/
+var script = modify('tommy', function(x) {
+  return x.toLocaleUpperCase();
+}).chain(function(_) {
+  return get('tommy');
+});
+
+console.log(pureObject(script, {tommy: 'howdy', tammy: 'aloha'}));
+
+var obj = {tommy: 'howdy', tammy: 'aloha'};
+console.log('%s', mutableObject(script, obj));
+console.log(obj);
---
+++ src/index.js
@@ -0,0 +1,81 @@
+'use strict';
+
+var pbp = require('pbp');
+var sum = require('ce-sum');
+var mixin = require('ce-mixin').mixin;
+
+var enumerableGet = pbp.enumerableGet;
+var enumerableValue = pbp.enumerableValue;
+var value = pbp.value;
+
+function underlying(func) {
+  return function(y) {
+    return this.ctor.apply(this, this.args.map(function(x) {
+      return x.map(function(free) {
+        return free[func](y);
+      });
+    }));
+  };
+};
+
+var Free = Object.create(sum.Either, {
+  of: enumerableValue(Of),
+  goCata: enumerableValue(function(obj) {
+    return this.go(function(x) {
+      return x.cata(obj);
+    });
+  }),
+});
+
+function Of(x) {
+  return mixin(sum.Right(x), Object.create(Free, {
+    type: value('Of'),
+    args: value([x]),
+    ctor: value(Of),
+    go: enumerableValue(function(_) {
+      return x;
+    }),
+  }));
+};
+
+function Join(x) {
+  return Object.create(Free, {
+    type: value('Join'),
+    args: value([x]),
+    ctor: value(Join),
+    map: enumerableValue(underlying('map')),
+    ap: enumerableValue(underlying('ap')),
+    chain: enumerableValue(underlying('chain')),
+    go: enumerableValue(function(f) {
+      return f(x).go(f);
+    }),
+  });
+};
+
+var FreeDerive = Object.create(null, {
+  liftF: enumerableGet(function() {
+    return liftF(this);
+  }),
+});
+
+function go(f, free) {
+  return free.go(f);
+}
+
+function goCata(obj, free) {
+  return free.goCata(obj);
+}
+
+function liftF(f) {
+  return Join(f.map(Of));
+}
+
+module.exports = {
+  Free: Free,
+  FreeDerive: FreeDerive,
+  go: go,
+  goCata: goCata,
+  Join: Join,
+  liftF: liftF,
+  Of: Of,
+};
